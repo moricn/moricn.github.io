3e9d8e46a57e92218164edf2e52f912d04e345c6
moricn <qiuyunfengdege@qq.com>
2024-09-25 23:54:45 +0800

    commit01

---
+++ src/main/java/com/moricn/MoricnWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.moricn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoricnWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoricnWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moricn/MoricnWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moricn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoricnWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
